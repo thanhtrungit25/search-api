282c866d31b674b891efd8f4dd1e7b50723561c4
TrungDT <thanhtrung.it25@gmail.com>
2019-07-29 18:47:36 +0700

    Add checkSearchParams check q parameter

--- src/services/search /routes.ts
+++ src/services/search /routes.ts
@@ -1,12 +1,14 @@
 import { Request, Response } from "express";
 import { HTTP401Error } from "../../utils/httpErrors";
-import { getPlacesByName } from './SearchController';
+import { getPlacesByName } from "./SearchController";
+import { checkSearchParams } from "../../middleware/checks";
 
 export default [
   {
     path: "/api/v1/search",
     method: "get",
     handler: [
+      checkSearchParams,
       async ({ query }: Request, res: Response) => {
         const result = await getPlacesByName(query.q);
         res.status(200).send(result);
