d0fc7c53e2bee45ce29d1eecb6d77c79ea9c3214
TrungDT <thanhtrung.it25@gmail.com>
2019-07-29 18:05:32 +0700

    Add SearchController, OpenCageData Provider

--- src/services/search /routes.ts
+++ src/services/search /routes.ts
@@ -1,15 +1,16 @@
 import { Request, Response } from "express";
 import { HTTP401Error } from "../../utils/httpErrors";
+import { getPlacesByName } from './SearchController';
 
 export default [
   {
-    path: "/",
+    path: "/api/v1/search",
     method: "get",
-    handler: async (req: Request, res: Response) => {
-      // res.send("Hello World!!!");
-      let err = new HTTP401Error();
-      console.log('err:', err)
-      throw err;
-    }
+    handler: [
+      async ({ query }: Request, res: Response) => {
+        const result = await getPlacesByName(query.q);
+        res.status(200).send(result);
+      }
+    ]
   }
 ];
