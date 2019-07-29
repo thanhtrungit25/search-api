748e2142ea3b3c7f1f2cf1280abc81ac2cc6d5ab
TrungDT <thanhtrung.it25@gmail.com>
2019-07-29 17:20:15 +0700

    add ErrorHandler

--- src/services/search /routes.ts
+++ src/services/search /routes.ts
@@ -1,11 +1,15 @@
 import { Request, Response } from "express";
+import { HTTP401Error } from "../../utils/httpErrors";
 
 export default [
   {
     path: "/",
     method: "get",
     handler: async (req: Request, res: Response) => {
-      res.send("Hello World!");
+      // res.send("Hello World!!!");
+      let err = new HTTP401Error();
+      console.log('err:', err)
+      throw err;
     }
   }
 ];
